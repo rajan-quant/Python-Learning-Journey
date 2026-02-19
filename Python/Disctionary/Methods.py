ec62bcd63673be3f851181d66a6de163d5ec6741
Rajan Thakur <mlrizzz800@gmail.com>
2026-02-19 12:10:07 +0545

    Refactor and update dictionary methods examples

--- Python/Disctionary/Methods.py
+++ Python/Disctionary/Methods.py
@@ -1,8 +1,9 @@
-#Methods
+#Methods of disctionary
 #1 - get()
+"""
 profile ={
     "name":"raju",'age':21,'salary':'30lakh',
-}
+}"""
 """age = profile.get('age1','Not Found')#if value is not in dict.. ie. Not Found is given
 print(age)"""
 
@@ -12,28 +13,43 @@ print(age)"""
 #print(list(keys)) #this will return a list of keys
 
 #3- values() if i just want to get the values 
+#eg:-
 """values = profile.values()
 print(list(values))
 """
 
 #4. items() to access both key and values
+#eg:-
 """
 items = profile.items()
 print(list(items))"""
 
 #5.pop() to delete a key from the dictionary
+#eg:-
 """poped = profile.pop('age','Not Found')
 print(poped)
 print(profile)"""
 
 #6.popitems() - delete the last key:pair of our dictionary
- 
+ #eg:-
 """
 popped_i = profile.popitem()
 print(popped_i)
 print(profile)"""
 
 #7.clear() - to delete all the key:value paris form the list
+#eg:-
+"""
 cleared = profile.clear()
 print(cleared)
-print(profile)
+
+print(profile)"""
+
+#8. zip() - join two list together and gives a single dictionary
+#eg:-
+"""
+keys = ['name','section','semester']
+values = ['Rajan','CSE-14','2nd']
+
+dis = dict(zip(keys, values))
+print(dis)"""
