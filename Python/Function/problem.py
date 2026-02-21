7ba84ebe00d8fdf47e477f276f69a04c52943225
Rajan Thakur <mlrizzz800@gmail.com>
2026-02-21 10:31:17 +0545

    Adding multiple solved problems of funcions
    Added multiple functions for mathematical operations and list manipulations, including square, larger number, even/odd check, factorial, digit count, reverse and palindrome check, maximum value, and duplicate removal.

--- Python/Function/problem.py
+++ Python/Function/problem.py
@@ -1,20 +1,121 @@
-"""a = 10
-b=20
-print(a+b)
+#Questions and  solving related to funcition Module in 'Python'.
 
-a = 30
-b=40
-print(a+b)"""
-#instead of this i will use function to use same block of code multiple times
+#🧠 PART 1: FUNCTIONS (Level: Beginner → Strong)
+#🟢 Basic Level
 
-#functions
-#1-reusibility
-#2-modularity
-#3-scoping
+# Q.Write a function that returns the square of a number.
+"""
+n = int(input('Enter a num:'))
+def square(n):
+    sqr = n*n
+    return sqr
+
+s = square(n)
+print(s)"""
+
+# Q. Write a function that takes two numbers and returns the larger one.
+"""
+num1 = int(input('Enter a num1: '))
+num2 = int(input('Enter another num2: '))
+
+def largeNum(num1,num2):
+    if num1>num2:
+        #print("NUM 1 is greater")
+        return num1
+    else:
+       # print("Num2 is greater")
+        return num2
+    
+large = largeNum(num1,num2)
+print("Larger Number is",large)"""
+
+# Q. Write a function that checks if a number is even or odd.
+"""
+num = int(input('Enter a num to check:'))
+
+def Even_or_Odd(num):
+    if num%2 ==0:
+        print("Number is Even")
+    else:
+        print("Num is Odd")
+
+Even_or_Odd(num)
+"""
+
+# Q. Write a function that returns the factorial of a number.
+"""
+num = int(input('Enter a num to check:'))
+
+def factorial(num):
+    if num ==0:
+        return 1
+    else:
+        return num*factorial(num-1)
+
+res = factorial(num)
+print(res)
+    """
+
+# Q. Write a function that counts how many digits are in a number.
+"""
+num = int(input('Enter a num to check:'))
+count = 0
+
+def counts(num):
+    num = abs(num)
+    return len(str(num))
+
+digit = counts(num)
+print("Digits in a Number: ",digit)
+"""
+
+#🟡 Intermediate Level
+
+# Q. Write a function that returns the reverse of a number and check it is a palindrome or not.
+"""
+num = int(input('Enter a num to reverse:'))
+def reverse(num):
+    rev = 0
+    org = num
+    while num>0:
+        rem = num%10
+        rev = rev*10+rem
+        num//=10
+    if org == rev:
+        print("It is palindrome")
+    else:
+        print("It is not a palindrome")
+    return rev
+
+res = reverse(num)
+print("Reverse of a number: ",res)"""
 
 
+# Q. Write a function that takes a list and returns the maximum value.
+"""
+lst = list(map(int,input('Enter values of list:').split()))
 
+def max_value(lst):
+    value = max(lst)
+    return value
+Max = max_value(lst)
+print("Maximum Value in List: ",Max)
+"""
 
-#Questions solving related to funcitions
+# Q. Write a function that removes duplicates from a list.
+"""
+lst = list(map(int,input('Enter values of list: ').split()))
 
-#1.
+copy = []
+dupli=[]
+
+def duplicate(lst):
+    for num in lst:
+        if num in copy:
+            dupli.append(num)
+        else:
+            copy.append(num)
+
+duplicate(lst)
+print("Removed Duplicate values list: ",copy)"""
+
